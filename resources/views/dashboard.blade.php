329185753cfc4bbf7dbd94ada9938b87df92b13a
Jose Antonio Dominguez Camacho <joseantonio.dominguez@iesdonana.org>
2025-01-14 21:00:53 +0100

    closes #RF1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,32 +1,26 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Meneame 2024/25') }}
-        </h2>
+        <div class="mx-auto h-7">
+            <x-primary-button class=" bg-orange-600">Crear noticia</x-primary-button>
+        </div>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        @foreach ($noticias as $noticia)
+            <div class="shadow-2xl bg-white w-2/4 border border-black  mx-auto rounded-xl p-2 mt-2">
 
-            <div class="overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    @foreach ($noticias as $noticia)
-                    <div class="bg-white shadow-2xl">
-                        <h1 class="text-xl p-2">
-                            {{$noticia->titulo}}
-                        </h1>
-                    </div>
+                <div class="flex">
+                    <h1 class="text-xl p-2">{{ $noticia->titulo }}</h1>
+                </div>
 
-                    <div class="text-xs p-4">
-                        Publicado por: {{$noticia->user->name}}
-                        Publicado el: {{$noticia->created_at->format('d-m-Y')}}
-                    </div>
-                        <p class="p-2">{{$noticia->contenido}}</p>
-                        <p class="p-2"> Categoria: {{$noticia->categoria->denominacion}}</p>
-                    @endforeach
+                <div class="bg-white rounded-xl">
+                    <p class="p-2">{{ $noticia->contenido }}</p>
+                </div>
+                <div class="text-xs p-4">
+                    Publicado por: {{$noticia->user->name}} |
+                                   {{$noticia->created_at->format('d-m-Y')}} |
+                                   {{ $noticia->categoria->denominacion }}
                 </div>
             </div>
-
-        </div>
+            @endforeach
     </div>
 </x-app-layout>
